326470e968701205217059e27171fc843084575e
Luki <>
2019-03-14 16:58:22 -0700

    I finished Magic 8 ball and Validation

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -1,35 +1,47 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2018
 //    Level 0
 
-
 package random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
+public static void main(String[] args) {
+	
 	// 1. Make a main method that includes all the steps below….
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+Random rand = new Random();
+ int r =rand.nextInt(4);// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
-	// 3. Print out this variable
+	System.out.println(r);// 3. Print out this variable
 
-	// 4. Get the user to enter a question for the 8 ball
+	JOptionPane.showInputDialog("enter a question for the magic 8 ball.");// 4. Get the user to enter a question for the 8 ball
 
-	// 5. If the random number is 0
+	if(r==0) {
+	JOptionPane.showMessageDialog(null, "YES!!");	// 5. If the random number is 0
+	}
 
 	// -- tell the user "Yes"
 
-	// 6. If the random number is 1
+	else if(r==1) {
+		JOptionPane.showMessageDialog(null, "NO!!");// 6. If the random number is 1
+	}
 
 	// -- tell the user "No"
 
-	// 7. If the random number is 2
-
+	else if(r==2) {
+		JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");// 7. If the random number is 2
+	}
 	// -- tell the user "Maybe you should ask Google?"
 
-	// 8. If the random number is 3
+	else{
+		JOptionPane.showMessageDialog(null, "MAYBE YOU SHOULD WRITE YOUR OWN AWNSER!!!");// 8. If the random number is 3
+	}
 
 	// -- write your own answer
 
+}
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,17 +9,31 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+		for(int i = 0; i<10;i++)	{
 		Random randomMaker = new Random();
 		
 		int randomNumber = randomMaker.nextInt(5);
-		
+	
 		System.out.println(randomNumber);
 
-		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if(randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "You are very good at coding");// 1. Use each value of randomNumber to give the user a random compliment.
+		}
+		else if(randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "You are a really nice person");
+		}
+		else if(randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "You are really good at drawing");	
+		}
+		else if(randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "You are a really helpful person");	
+		}
+		else {
+		JOptionPane.showMessageDialog(null, "You are very smart");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
+}
 }
